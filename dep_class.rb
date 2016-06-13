8e4e0e4dc7508bc042838a6283d6561be256f547
mazeem3 <maaz.azeem3@yahoo.com>
2016-06-13 09:26:41 -0500

    Made changes to test

--- dep_class.rb
+++ dep_class.rb
@@ -1,5 +1,8 @@
 class Department < Employee
   attr_accessor :dept_name
+  def initialize(dept_name)
+    @dept_name=dept_name
+  end
   def add_emp
 
   end
--- emp_class.rb
+++ emp_class.rb
@@ -1,6 +1,6 @@
 class Employee
   attr_accessor :name, :email, :phone, :salary
-  def create_new_employee(name, email, phone, salary)
+  def initialize(name, email, phone, salary)
     @name = :name
     @email = :email
     @phone = :phone
